4635f6c4988d17e545a8db66933f0454ce1909cd
medkov77 <medkov77@mail.rum>
2022-06-22 20:12:14 +0300

    delete task

---
+++ src/store/actions.js
@@ -0,0 +1,21 @@
+// import { type } from "@testing-library/user-event/dist/type";
+import * as actionTypes from "./actionTypes";
+
+export function taskCompleted(id) {
+  return {
+    type: actionTypes.taskUpdated,
+    payload: { id, completed: true },
+  };
+}
+export function titleChanged(id) {
+  return {
+    type: actionTypes.taskUpdated,
+    payload: { id, title: `New title for ${id}` },
+  };
+}
+export function taskDelete(id) {
+  return {
+    type: actionTypes.taskDelete,
+    payload: { id },
+  };
+}
---
+++ src/store/taskReducer.js
@@ -0,0 +1,25 @@
+import { taskUpdated, taskDelete } from "./actionTypes";
+
+export function taskReducer(state = [], action) {
+  switch (action.type) {
+    case taskUpdated: {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex] = {
+        ...newArray[elementIndex],
+        ...action.payload,
+      };
+      return newArray;
+    }
+
+    case taskDelete: {
+      console.log(action.payload.id);
+      const newArray = [...state].filter((el) => el.id !== action.payload.id);
+      return newArray;
+    }
+    default:
+      return state;
+  }
+}
